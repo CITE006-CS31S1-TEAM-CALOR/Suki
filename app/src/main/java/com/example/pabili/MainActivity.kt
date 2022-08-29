93632873304f6049d141b33e2849a3103c0ff2ad
root <mrpmartinez@tip.edu.ph>
2022-08-30 00:21:06 +0800

    initial commit

---
+++ app/src/main/java/com/example/pabili/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.pabili
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter:RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        layoutManager = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager  = layoutManager
+        adapter = RecyclerAdapter()
+        recyclerView.adapter = adapter
+    }
+}   
