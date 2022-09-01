91d422201ac083d2c72f584cc4e908e8069423b2
root <mrpmartinez@tip.edu.ph>
2022-09-02 04:37:07 +0800

    login firestore and fix in recyclerview

---
+++ app/src/main/java/com/example/pabili/LoginActivity.kt
@@ -0,0 +1,64 @@
+package com.example.pabili
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Button
+import android.widget.EditText
+import android.content.Intent
+import android.view.View
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        	
+	//try {]	
+		
+		val db = FirebaseFirestore.getInstance()
+		val etUsername: EditText = findViewById(R.id.etUsername) as EditText 
+		val etPassword: EditText = findViewById(R.id.etPassword) as EditText 
+		val btnCustomerLogin: Button = findViewById(R.id.btnCustomerLogin)
+		val btnStoreLogin: Button = findViewById(R.id.btnStoreLogin)
+
+		btnCustomerLogin.setOnClickListener {
+			var username = etUsername.text.toString()
+			var password = etPassword.text.toString()
+			db.collection("users")
+			.whereEqualTo("username", username).whereEqualTo("password", password)
+			.get()
+			.addOnSuccessListener { documents ->
+			    for (document in documents) {
+				Toast.makeText(this@LoginActivity,"Access Granted",Toast.LENGTH_SHORT).show()
+				 val intent = Intent(this, MainActivity::class.java).apply {
+					    putExtra(EXTRA_MESSAGE, username)
+				 }
+				 startActivity(intent)				
+			    }
+			}
+		}
+		
+		btnStoreLogin.setOnClickListener {
+			var username = etUsername.text.toString()
+			var password = etPassword.text.toString()
+			db.collection("stores")
+			.whereEqualTo("username", username).whereEqualTo("password", password)
+			.get()
+			.addOnSuccessListener { documents ->
+			    for (document in documents) {
+				Toast.makeText(this@LoginActivity,"Access Granted",Toast.LENGTH_SHORT).show()
+			    }
+			}
+		}
+		
+		
+			
+	/*			
+	} catch (e: IOException) {
+		Toast.makeText(this@LoginActivity,"ttt",Toast.LENGTH_SHORT).show()
+	}*/
+    }
+}
--- app/src/main/java/com/example/pabili/MainActivity.kt
+++ app/src/main/java/com/example/pabili/MainActivity.kt
@@ -9,12 +9,24 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
+import android.content.Intent
 import City
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.text.Editable
+import android.text.TextWatcher
+
+
+
 class MainActivity : AppCompatActivity() {
+
+    
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter:RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
     private var customer_id: Int? = null;
@@ -23,76 +35,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+	val currentUser = intent.getStringExtra(EXTRA_MESSAGE)
+	
+	Toast.makeText(this@MainActivity,"Hello $currentUser",Toast.LENGTH_SHORT).show()
         layoutManager = LinearLayoutManager(this)
-        customer_id = 1
         
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager  = layoutManager
         adapter = RecyclerAdapter()
         recyclerView.adapter = adapter
-	readDataFromFirestore()
-	
+        
+        
+    	var btnScanner = findViewById<Button>(R.id.btnScanner)
+    	btnScanner.setOnClickListener {
+
+		val intent = Intent(this, ScannerActivity::class.java).apply {
+	        }
+		 startActivity(intent)	    	
+    	
+    	
+    	}
     }
-    
-    
- 
-     private fun readDataFromFirestore() {
-		val db = FirebaseFirestore.getInstance()
-		val docRef = db.collection("Database").document("orders")
-		
-		val docData = hashMapOf(
-        "stringExample" to "Hello world!",
-        "booleanExample" to true,
-        "numberExample" to 3.14159265,
-        "listExample" to arrayListOf(1, 2, 3),
-        "nullExample" to null
-		)
+}
 
-	val nestedData = hashMapOf(
-		"a" to 5,
-		"b" to true
-	)
-
-	docData["objectExample"] = nestedData
-
-	db.collection("Database").document("test")
-		.set(docData)
-		.addOnSuccessListener {  }
-		.addOnFailureListener {  }
-	
-	val data5 = hashMapOf(
-        "name" to "Beijing",
-        "state" to null,
-        "country" to "China",
-        "capital" to true,
-        "population" to 21500000,
-        "regions" to listOf("jingjinji", "hebei")
-	)
-	db.collection("Database").document("BJ").set(data5)
-
-val city = City("Los Angeles", "CA", "USA",
-        false, 5000000L, listOf("west_coast", "socal"))
-db.collection("Database").document("LA").set(city)
-
-	val docRef1 = db.collection("Database").document("BJ")
-	docRef1.get().addOnSuccessListener { documentSnapshot ->
-	    val city = documentSnapshot.toObject<City>(City::class.java)
-	    Toast.makeText(this@MainActivity,city?.regions.toString(),Toast.LENGTH_SHORT).show()
-	}
-		//Writing
-		
-		//Reading
-		/*	
-		docRef.get().addOnSuccessListener { document -> 
-			if (document != null){
-				Toast.makeText(this@MainActivity,"Document Snapshot data: ${document.data}",Toast.LENGTH_SHORT).show()
-			} else {
-				Toast.makeText(this@MainActivity,"No such document",Toast.LENGTH_SHORT).show()
-			}
-			/*.addOnFailureListener { exception ->
-				Toast.makeText(this@MainActivity,"get failed with",Toast.LENGTH_SHORT).show()
-			}*/
-		}*/
-   	 }
-
-}   
