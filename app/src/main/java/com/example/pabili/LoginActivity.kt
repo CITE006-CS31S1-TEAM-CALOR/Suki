f5484511cd85506d9e32180df8947af27fa0e205
Jean <tonido.caryl@gmail.com>
2022-09-14 22:42:16 +0800

    Animation for Login Buttons. New Anim XML Files. - Tonido

--- app/src/main/java/com/example/pabili/LoginActivity.kt
+++ app/src/main/java/com/example/pabili/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pabili
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.firestore.FirebaseFirestore
@@ -8,17 +9,23 @@ import android.widget.Toast
 import android.widget.Button
 import android.widget.EditText
 import android.content.Intent
+import android.view.MotionEvent
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.app.NotificationCompat.getAction
+import androidx.core.view.accessibility.AccessibilityEventCompat.getAction
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 class LoginActivity : AppCompatActivity() {
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-            
+
     //try {]    
-    
-            
+
         val etUsername: EditText = findViewById(R.id.etUsername)
         val etPassword: EditText = findViewById(R.id.etPassword)
         val btnCustomerLogin: Button = findViewById(R.id.btnCustomerLogin)
@@ -26,6 +33,57 @@ class LoginActivity : AppCompatActivity() {
         val btnSignup: Button = findViewById(R.id.btnSignup)
 
         val db = FirebaseFirestore.getInstance()
+
+        val scaleUp: Animation = AnimationUtils.loadAnimation(this,R.anim.scale_up)
+        val scaleDown: Animation = AnimationUtils.loadAnimation(this,R.anim.scale_down)
+
+        btnCustomerLogin.setOnTouchListener(object:View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean{
+                when (v) { v ->
+                    when (event?.action) { MotionEvent.ACTION_UP -> {
+                        btnCustomerLogin.startAnimation(scaleDown)
+                    } MotionEvent.ACTION_DOWN -> {
+                        btnCustomerLogin.startAnimation(scaleUp)
+                    }
+                }
+
+                }
+                return v?.onTouchEvent(event) ?: true
+            }
+
+        })
+
+        btnStoreLogin.setOnTouchListener(object:View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean{
+                when (v) { v ->
+                    when (event?.action) { MotionEvent.ACTION_UP -> {
+                        btnStoreLogin.startAnimation(scaleDown)
+                    } MotionEvent.ACTION_DOWN -> {
+                        btnStoreLogin.startAnimation(scaleUp)
+                    }
+                    }
+
+                }
+                return v?.onTouchEvent(event) ?: true
+            }
+
+        })
+
+        btnSignup.setOnTouchListener(object:View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean{
+                when (v) { v ->
+                    when (event?.action) { MotionEvent.ACTION_UP -> {
+                        btnSignup.startAnimation(scaleDown)
+                    } MotionEvent.ACTION_DOWN -> {
+                        btnSignup.startAnimation(scaleUp)
+                    }
+                    }
+
+                }
+                return v?.onTouchEvent(event) ?: true
+            }
+
+        })
 
         btnCustomerLogin.setOnClickListener {
             var username = etUsername.text.toString()
