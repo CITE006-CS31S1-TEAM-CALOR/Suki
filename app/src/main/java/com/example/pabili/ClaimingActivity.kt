13c19d9aa8b0b9d9fbfdebb46e1a866bcc4f7e7b
alfrdfrd <roialvarico@gmail.com>
2022-09-17 14:47:51 +0800

    Cleaned up ClaimingActivity.kt and added Toast message if there were no transactionID received

--- app/src/main/java/com/example/pabili/ClaimingActivity.kt
+++ app/src/main/java/com/example/pabili/ClaimingActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import com.google.firebase.firestore.FirebaseFirestore
 import android.widget.TextView
 import android.widget.ImageView
+import android.widget.Toast
 
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,14 +18,10 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 class ClaimingActivity: AppCompatActivity() {
 
-	private lateinit var cInfo: DataCustomerInfo
     override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_claiming)
-
 		val db = FirebaseFirestore.getInstance()
-
-
 		val transactionId = intent.getStringExtra("transactionId")
 		val tvTransactionId = findViewById<TextView>(R.id.tvTransactionId)
 		tvTransactionId.text = transactionId
@@ -39,12 +36,11 @@ class ClaimingActivity: AppCompatActivity() {
 		val totaltxt = findViewById<TextView>(R.id.totalText)
 		val status = findViewById<TextView>(R.id.txtStatus)
 		val storeN = findViewById<TextView>(R.id.txtStore)
-
 		db.collection("orders").whereEqualTo("transactionID", transactionId)
 			.get()
 			.addOnSuccessListener { result ->
 				if(result.isEmpty){
-					Log.d("FAIL", "No data")
+					Toast.makeText(this, ("No orders received"), Toast.LENGTH_SHORT).show()
 				} else{
 					for(document in result){
 						val cDoc = document.id
@@ -71,84 +67,11 @@ class ClaimingActivity: AppCompatActivity() {
 					}
 				}
 			}
-
 			val editBtn = findViewById<Button>(R.id.btnEdit)
-
 			editBtn.setOnClickListener{
 				super.finish()
 			}
-
-
 	}
-
-
-
 }
 
-
-
-//			db.collection("orders")
-//				.get()
-//				.addOnCompleteListener {
-//					task ->
-//						if(task.isSuccessful){
-//							for(document in task.result){
-//
-//							}
-//
-//
-//
-//							val adapter = RecyclerOrder(data)
-//							recyclerView.adapter = adapter
-//						}
-//					Log.d("TAG", "Orders: $oL")
-//					}
-
-
-
-
-
-	//try {]	
-		/*
-		val db = FirebaseFirestore.getInstance()
-		val etUsername: EditText = findViewById(R.id.etUsername) as EditText 
-		val etPassword: EditText = findViewById(R.id.etPassword) as EditText 
-		val btnCustomerLogin: Button = findViewById(R.id.btnCustomerLogin)
-		val btnStoreLogin: Button = findViewById(R.id.btnStoreLogin)
-
-		btnCustomerLogin.setOnClickListener {
-			var username = etUsername.text.toString()
-			var password = etPassword.text.toString()
-			db.collection("users")
-			.whereEqualTo("username", username).whereEqualTo("password", password)
-			.get()
-			.addOnSuccessListener { documents ->
-			    for (document in documents) {
-				Toast.makeText(this@LoginActivity,'Access Granted',Toast.LENGTH_SHORT).show()
-				 val intent = Intent(this, MainActivity::class.java).apply {
-					    putExtra(EXTRA_MESSAGE, username)
-				 }
-				 startActivity(intent)				
-			    }
-			}
-		}
-		
-		btnStoreLogin.setOnClickListener {
-			var username = etUsername.text.toString()
-			var password = etPassword.text.toString()
-			db.collection("stores")
-			.whereEqualTo("username", username).whereEqualTo("password", password)
-			.get()
-			.addOnSuccessListener { documents ->
-			    for (document in documents) {
-				Toast.makeText(this@LoginActivity,"Access Granted",Toast.LENGTH_SHORT).show()
-			    }
-			}
-		}
-		
-		
-			
-	} catch (e: IOException) {
-		Toast.makeText(this@LoginActivity,"ttt",Toast.LENGTH_SHORT).show()
-	}*/
     
